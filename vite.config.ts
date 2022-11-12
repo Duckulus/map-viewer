3f2b598ffb8019ed70124fe10faaaf6912b4e1db
Amin <amin.haddou@frg.wwschool.de>
2022-11-12 15:29:54 +0100

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+export default defineConfig(({mode}) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    return {
+      plugins: [solidPlugin()],
+      base: process.env.VITE_BASE_URL,
+      server: {
+        port: 3000,
+      },
+      build: {
+        target: 'esnext',
+      },
+    }
 });
